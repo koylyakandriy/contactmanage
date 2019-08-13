d61159f849e00de2472fe435c723726cccffbe17
Andriy Koylyak <koylyakandriy.com>
2019-08-13 22:38:39 +0300

    Updates

---
+++ precache-manifest.2dfc3320713b80bcdfab0d89f0734fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f2625e86a8801351d35d71c5691463",
+    "url": "/contactmanage/index.html"
+  },
+  {
+    "revision": "3b1b3bff2b7e101b830a",
+    "url": "/contactmanage/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5a1c236ee12d2a0389f2",
+    "url": "/contactmanage/static/css/main.fe736b1a.chunk.css"
+  },
+  {
+    "revision": "3b1b3bff2b7e101b830a",
+    "url": "/contactmanage/static/js/2.292b09d7.chunk.js"
+  },
+  {
+    "revision": "5a1c236ee12d2a0389f2",
+    "url": "/contactmanage/static/js/main.694445c8.chunk.js"
+  },
+  {
+    "revision": "569a905144fa3ba69dae",
+    "url": "/contactmanage/static/js/runtime~main.b6909396.js"
+  }
+]);
